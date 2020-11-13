278be9e12dff10f7068c473dc8959f8d691eede7
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2020-11-13 14:04:17 +0700

    Add loading page will show when browse load a page

---
+++ index.js
@@ -0,0 +1,18 @@
+
+(function ($) {
+    "use strict";
+
+    // loading page
+    $(window).on('load', function () {
+        var preloaderFadeOutTime = 500;
+        function hidePreloader() {
+            var preloader = $('.spinner-wrapper');
+            setTimeout(function () {
+                preloader.fadeOut(preloaderFadeOutTime);
+            }, 500);
+        }
+        hidePreloader();
+    });
+
+
+})(jQuery);
