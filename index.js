b09efe247ed5d70a5e6760b41ba97263e81ef303
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2020-11-17 14:36:35 +0700

    add footer and format some code

--- index.js
+++ index.js
@@ -22,13 +22,17 @@
             $("nav").removeClass("navbar-dark");
             $("#logoPojok").addClass("brand-dark");
             $("#logoPojok").removeClass("brand-light");
+            $("#posisilogo").removeClass("navbar-brand-position");
+            $("#navbarsExampleDefault").removeClass("navbar-link-position");
+
         }
         else {
             $("nav").removeClass("bg-light");
             $("nav").addClass("navbar-dark");
             $("#logoPojok").addClass("brand-light");
             $("#logoPojok").removeClass("brand-dark");
-            
+            $("#posisilogo").addClass("navbar-brand-position");
+            $("#navbarsExampleDefault").addClass("navbar-link-position");
 
         }
     });
